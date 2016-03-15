df593b9c16b6089ed743287e0f9c86665111c4c3
hemant0071234 <hemant.janrao@xoriant.com>
2016-03-15 16:25:13 +0530

    Updated app name

--- dbHost.sh
+++ dbHost.sh
@@ -2,4 +2,4 @@
 # My first script
 
 echo "Storing DB host values"
-PG_HOST=$(docker inspect testttt_postgresnewtest6_run_1 | grep "\"IPAddress\"" | cut -d ':' -f 2| cut -d '"' -f 2)
+PG_HOST=$(docker inspect testttt_postgresnew_run_1 | grep "\"IPAddress\"" | cut -d ':' -f 2| cut -d '"' -f 2)
